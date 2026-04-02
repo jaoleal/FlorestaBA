Micah-Shallom/Floresta

feat(wire): implement BanMan for centralized ban management
Introduce a BanMan struct that centralizes IP ban tracking,
replacing the scattered ban logic across AddressMan and peer_man.

- Add BanMan with add_ban() and is_banned() methods
- Wire BanMan into NodeCommon alongside AddressMan
- Check banned status before creating new connections
- Register bans in both disconnect_and_ban() and increase_banscore()
- Add PeerBanned variant to WireError

Closes #820 (partial — single-IP banning only)

crates/floresta-wire/src/p2p_wire/node/conn.rs
@@ -90,6 +90,11 @@ where
             return Err(WireError::NoAddressesAvailable);
         };
 
+        let net_address = address.get_net_address();
+        if self.ban_man.is_banned(net_address) {
+            return Err(WireError::PeerBanned(net_address));
+        }
+
         debug!("attempting connection with address={address:?} kind={kind:?}",);
 
         let now = SystemTime::now()

jaoleal/FlorestaBA

crates/floresta-wire/src/p2p_wire/node/conn.rs
@@ -106,6 +106,11 @@ where
             return Err(WireError::NoAddressesAvailable);
         };
 
+        let net_address = peer_address.get_net_address();
+        if self.ban_man.is_banned(net_address) {
+            return Err(WireError::PeerBanned(net_address));
+        }
+
         debug!("Attempting connection with address={peer_address:?} kind={conn_kind:?}",);
 
         let now = SystemTime::now()